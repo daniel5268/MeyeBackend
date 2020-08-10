915e18515c1d35eeece6719c115427280a7fcfeb
NECSOFT <necsoft@MacBook-Pro-de-NECSOFT.local>
2020-08-10 16:24:41 -0500

    create pj endpoint and tests

--- tests/utils/DataBaseTestUtils.js
+++ tests/utils/DataBaseTestUtils.js
@@ -4,12 +4,18 @@ const knex = require('knex');
 
 const EncryptionService = require('../../src/services/EncryptionService');
 const { dataBaseConfig } = require('../../src/config');
-const { USERS } = require('../../src/repositories/TableNames');
+const { USERS, PJS } = require('../../src/repositories/TableNames');
 const UsersTestData = require('../data/UsersTestData');
 
 const db = knex(dataBaseConfig);
 
-DataBaseTestUtils.cleanDataBase = () => db(USERS).truncate();
+DataBaseTestUtils.cleanDataBase = async () => {
+  await db(PJS).delete();
+  await db(USERS).delete();
+
+  await DataBaseTestUtils.resetId(USERS);
+  await DataBaseTestUtils.resetId(PJS);
+};
 
 DataBaseTestUtils.insertInitialTestData = async () => {
   const userToInsert = { ...UsersTestData.user, secret: await EncryptionService.hash(UsersTestData.user.secret) };
@@ -18,6 +24,8 @@ DataBaseTestUtils.insertInitialTestData = async () => {
 
   return { user_id: userId };
 };
+
+DataBaseTestUtils.resetId = (tableName) => db.schema.raw(`ALTER SEQUENCE ${tableName}_id_seq RESTART WITH 1`);
 
 DataBaseTestUtils.cleanRecord = (record, removeId = true) => {
   const { created_at: createdAt, updated_at: updatedAt, ...cleanedRecord } = record;
