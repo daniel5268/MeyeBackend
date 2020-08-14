081235de4e3e4fb0b00c1ce725ee276a3f23546c
Daniel Tamayo <daniel.tamayo@rappi.com>
2020-08-13 21:05:33 -0500

    specialties and specialty_ownerships tables

---
+++ migrations/20200813205610_create_specialty_ownerships_table.js
@@ -0,0 +1,14 @@
+const { SPECIALTY_OWNERSHIPS, PJS, SPECIALTIES } = require('../src/repositories/TableNames');
+const DataBaseUpdateTrigger = require('../src/utils/DataBaseUpdateTrigger');
+
+exports.up = (knex) => knex.schema.createTable(SPECIALTY_OWNERSHIPS, (table) => {
+  table.increments('id');
+  table.integer('pj_id').notNullable().references('id').inTable(PJS)
+    .onDelete('CASCADE');
+  table.integer('specialty_id').notNullable().references('id').inTable(SPECIALTIES)
+    .onDelete('CASCADE');
+  table.integer('value').notNullable();
+  table.timestamps(true, true);
+}).then(() => knex.raw(DataBaseUpdateTrigger(SPECIALTY_OWNERSHIPS)));
+
+exports.down = (knex) => knex.schema.dropTable(SPECIALTY_OWNERSHIPS);
--- src/repositories/TableNames.js
+++ src/repositories/TableNames.js
@@ -2,4 +2,6 @@ module.exports = {
   USERS: 'users',
   PJS: 'pjs',
   XP_ASSIGNATIONS: 'xp_assignations',
+  SPECIALTIES: 'specialties',
+  SPECIALTY_OWNERSHIPS: 'specialty_ownerships',
 };
